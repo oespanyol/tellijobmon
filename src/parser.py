4760b254e1dc77d71fc0708a85a71897729fe842
Oriol Espanyol <oespanyol@gmail.com>
2018-10-02 12:26:58 +0200

    Added entries for the database

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+import re
+# SQLAlchemy
+from sqlalchemy import Column, Integer, String, Boolean, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 __author__    = "Oriol Espanyol"
 __copyright__ = "Copyright 2018, EUMETCast Terrestrial Pathfinder-II"
 __license__   = "GPL"
@@ -7,10 +14,10 @@ __version__   = "1.0.1"
 __email__     = "oriol.espanyol@eumetsat.int"
 __status__    = "Development"
 
-import re
-import sqlalchemy
+Base = declarative_base()
 
-def parse(filepath):
+
+def parse(pfilepath):
     """
     Parse text at given filepath
 
@@ -20,9 +27,9 @@ def parse(filepath):
 
     """
 
-    with open(filepath, 'r') as file:
-        text = file.read()
-        file.close()
+    with open(pfilepath, 'r') as pfile:
+        text = pfile.read()
+        pfile.close()
 
         reg_match = _RegExLib(text)
 
@@ -37,6 +44,8 @@ def parse(filepath):
         # Put all contents together in one list
         job_contents = [channel_content, accounting_content, scheduling_content, recipients_content, files_content] 
 
+        # Instantiate a the job dictionary to keep all the keys and values
+        job_dict = {}
         # Iterate through the list of contents to prepend the name of the section
         for content in job_contents:
             prefix = content[0]
@@ -45,11 +54,24 @@ def parse(filepath):
                 if line.strip():
                     # Prepend the name of the section
                     section_line = prefix + '_' + line
-                    print(section_line)
+                    # Split line on "=" separator to generate the key value pair
+                    job_kvp = section_line.split("=", 2)
+                    job_dict[job_kvp[0]] = job_kvp[1]
 
-        #for filel in reg_match.filel:
-            #filel_content = filel.group('filel_content')
-            #print(filel_content)
+        session = _InitPersist().session
+
+        job = Job(job_dict)
+        print(job)
+
+        session.add(job)
+        my_job = session.query(Job).filter_by(channel_name='T01-EPS-A-11').first()
+        print(my_job)
+
+#        for filel in reg_match.filel:
+#            filel_content = filel.group('filel_content')
+#            print(filel_content)
+    return text
+
 
 class _RegExLib:
     """
@@ -75,7 +97,95 @@ class _RegExLib:
         self.filel      = self._reg_filel      .finditer(text)
 
 
+class Job(Base):
+    __tablename__ = 'jobs'
+
+    id = Column(Integer, primary_key=True)
+    channel_name                                = Column(String )
+    accounting_customer                         = Column(String )
+    scheduling_allow_recipient_modifications    = Column(String)
+    scheduling_start_time                       = Column(String   ) #2018-09-24 07:07:26
+    scheduling_priority                         = Column(Integer)
+    scheduling_retransmission_interval          = Column(Integer)
+    scheduling_start_time_window                = Column(Integer)
+    scheduling_nr_of_transmissions              = Column(Integer)
+    scheduling_status_keep_time                 = Column(Integer)
+    scheduling_expire_time                      = Column(String   ) #2018-09-24 07:08:29
+    scheduling_retransmission_type              = Column(String )
+    scheduling_acknowledgement_interval         = Column(Integer)
+    scheduling_loss_rate_threshold              = Column(Integer)
+    scheduling_request_acknowledgements         = Column(Integer)
+    scheduling_atomicity                        = Column(Integer)
+    scheduling_client_file_database_expire_time = Column(String   ) #2018-09-24 07:08:29
+
+    def __init__(self, jobdict):
+        self.channel_name = jobdict['channel_name']
+        self.accounting_customer = jobdict['accounting_customer']
+        self.scheduling_allow_recipient_modifications = jobdict['scheduling_allow_recipient_modifications']
+        self.scheduling_start_time = jobdict['scheduling_start_time'] #Column(Date)  # 2018-09-24 07:07:26
+        self.scheduling_priority = int(jobdict['scheduling_priority'])
+        self.scheduling_retransmission_interval = int(jobdict['scheduling_retransmission_interval'])
+        self.scheduling_start_time_window = int(jobdict['scheduling_start_time_window'])
+        self.scheduling_nr_of_transmissions = int(jobdict['scheduling_nr_of_transmissions'])
+        self.scheduling_status_keep_time = int(jobdict['scheduling_status_keep_time'])
+        self.scheduling_expire_time = jobdict['scheduling_expire_time'] #Column(Date)  # 2018-09-24 07:08:29
+        self.scheduling_retransmission_type = jobdict['scheduling_retransmission_type']
+        self.scheduling_acknowledgement_interval = int(jobdict['scheduling_acknowledgement_interval'])
+        self.scheduling_loss_rate_threshold = int(jobdict['scheduling_loss_rate_threshold'])
+        self.scheduling_request_acknowledgements = int(jobdict['scheduling_request_acknowledgements'])
+        self.scheduling_atomicity = int(jobdict['scheduling_atomicity'])
+        self.scheduling_client_file_database_expire_time = jobdict['scheduling_client_file_database_expire_time'] #Column(Date)
+
+    def __repr__(self):
+        return "<Job(" \
+               "channel_name='%s', " \
+               "accounting_customer='%s," \
+               "scheduling_allow_recipient_modifications='%s', "\
+               "scheduling_start_time='%s," \
+               "scheduling_priority='%u', "\
+               "scheduling_retransmission_interval='%u', "\
+               "scheduling_start_time_window='%u', "\
+               "scheduling_nr_of_transmissions='%u', "\
+               "scheduling_status_keep_time='%u', "\
+               "scheduling_expire_time = '%s," \
+               "scheduling_retransmission_type='%s', " \
+               "scheduling_acknowledgement_interval='%u', "\
+               "scheduling_loss_rate_threshold='%u', "\
+               "scheduling_request_acknowledgements='%u', "\
+               "scheduling_atomicity='%u', "\
+               "scheduling_client_file_database_expire_time='%s' "\
+               "')>" % (self.channel_name,
+                        self.accounting_customer,
+                        self.scheduling_allow_recipient_modifications,
+                        self.scheduling_start_time,
+                        self.scheduling_priority,
+                        self.scheduling_retransmission_interval,
+                        self.scheduling_start_time_window,
+                        self.scheduling_nr_of_transmissions,
+                        self.scheduling_status_keep_time,
+                        self.scheduling_expire_time,
+                        self.scheduling_retransmission_type,
+                        self.scheduling_acknowledgement_interval,
+                        self.scheduling_loss_rate_threshold,
+                        self.scheduling_request_acknowledgements,
+                        self.scheduling_atomicity,
+                        self.scheduling_client_file_database_expire_time
+                        )
+
+
+class _InitPersist:
+    """
+     Initiates the Persistence with SQLAlchemy
+    """
+    _engine = create_engine('sqlite:///:memory:', echo=True)
+    Base.metadata.drop_all(_engine)
+    Base.metadata.create_all(_engine)
+    _Session = sessionmaker(bind=_engine)
+
+    def __init__(self):
+        self.session = self._Session()
+
+
 if __name__ == '__main__':
-    
     filepath = 'sample.job'
     data = parse(filepath)
