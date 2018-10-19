a85a65cab8db0a09faff32c7262db6198f9e23de
Oriol Espanyol <oespanyol@gmail.com>
2018-10-19 16:15:33 +0200

    Remove old classes from previous example

--- sqlalchemy-datatables/examples/pyramid_tut/pyramid_tut/models.py
+++ sqlalchemy-datatables/examples/pyramid_tut/pyramid_tut/models.py
@@ -14,51 +14,6 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class User(Base):
-
-    """Define a User."""
-
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode, unique=True)
-    birthday = Column(Date)
-    address = relationship('Address', uselist=False, backref=backref('user'))
-
-    # calculating age from date is a bit hacky with sqlite
-    age = column_property(
-        (func.strftime('%Y.%m%d', 'now') -
-         func.strftime('%Y.%m%d', birthday)).cast(Integer)
-    )
-
-    def __unicode__(self):
-        """Give a readable representation of an instance."""
-        return '%s' % self.name
-
-    def __repr__(self):
-        """Give a unambiguous representation of an instance."""
-        return '<%s#%s>' % (self.__class__.__name__, self.id)
-
-
-class Address(Base):
-
-    """Define an Address."""
-
-    __tablename__ = 'addresses'
-
-    id = Column(Integer, primary_key=True)
-    description = Column(Unicode, unique=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-    def __unicode__(self):
-        """Give a readable representation of an instance."""
-        return '%s' % (self.id)
-
-    def __repr__(self):
-        """Give a unambiguous representation of an instance."""
-        return '<%s#%s>' % (self.__class__.__name__, self.id)
-
-
 class Job(Base, json.JSONEncoder):
     __tablename__ = 'jobs'
     id = Column(Integer, primary_key=True, autoincrement=True)
